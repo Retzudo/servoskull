35df16d36bcd727825c968ab3ed09a755268cb07
Retzudo <christoph.willinger@gmail.com>
2017-10-27 12:46:11 +0200

    Playing YouTube links. WIP: manage sound players better

--- servoskull/commands/sound.py
+++ servoskull/commands/sound.py
@@ -7,6 +7,9 @@ from servoskull.commands.regular import Command
 from servoskull.settings import USE_AVCONV
 
 
+_current_volume = 1.0
+
+
 class SoundCommand(Command):
     """Base class for commands that require the bot
     to be connected to a voice channel."""
@@ -17,14 +20,32 @@ class SoundCommand(Command):
     async def execute(self) -> str:
         """We override the `execute` method of `Command` and run a check before every execution
         if the bot is connected to a voice channel."""
-        if not self._get_voice_client():
+        if not self.voice_client:
             return 'I am not connected to any voice channel. Use `summon` to have me connect to one.'
         else:
             return await self.execute_sound()
 
-    def _get_voice_client(self):
+    @property
+    def voice_client(self):
         """Return the currently connected voice client of the message's server."""
-        return self.message.server.voice_client
+        voice_client = self.message.server.voice_client
+        if voice_client and not hasattr(voice_client, 'current_player'):
+            voice_client.current_player = None
+
+        return voice_client
+
+    @property
+    def current_player(self):
+        if self.voice_client:
+            return self.voice_client.current_player
+
+    @current_player.setter
+    def current_player(self, value):
+        if self.voice_client:
+            self.voice_client.current_player = value
+
+    def _unset_current_player(self):
+        self.current_player = None
 
 
 @registry.register('summon', sound=True)
@@ -56,21 +77,19 @@ class CommandSummon(SoundCommand):
         if not voice_channel:
             return 'You are not connected to any voice channel'
 
-        voice_client = self._get_voice_client()
-
         # Sometimes the client can't connect to voice channels due to
         # unknown network problems.
         try:
-            if not voice_client:
+            if not self.voice_client:
                 await self.client.join_voice_channel(voice_channel)
             else:
-                await voice_client.move_to(voice_channel)
+                await self.voice_client.move_to(voice_channel)
             user_name = connect_to_member.nick or connect_to_member.name
             return 'Connected to {}\'s voice channel "{}"'.format(user_name, voice_channel.name)
 
         except ConnectionResetError as e:
-            if voice_client:
-                voice_client.disconnect()
+            if self.voice_client:
+                self.voice_client.disconnect()
             return 'Could not connect to your voice channel: {}'.format(e)
 
 
@@ -80,8 +99,7 @@ class CommandDisconnect(SoundCommand):
 
     async def execute_sound(self):
         """Disconnect from the current voice channel."""
-        voice_client = self._get_voice_client()
-        await voice_client.disconnect()
+        await self.voice_client.disconnect()
 
 
 @registry.register('sound', sound=True)
@@ -93,14 +111,12 @@ class CommandSound(SoundCommand):
     sounds = {
         'horn': {
             'url': 'https://www.youtube.com/watch?v=9Jz1TjCphXE',
-            'volume': 0.1,
+            'volume': 0.2,
         },
     }
 
     async def execute_sound(self) -> str:
         """Play a sound."""
-        voice_client = self._get_voice_client()
-
         if not self.arguments or len(self.arguments) < 1:
             return 'Use `sounds` for a list of sounds.'
 
@@ -109,13 +125,17 @@ class CommandSound(SoundCommand):
         if not sound:
             return 'No such sound "{}". Use `sounds` for a list of sounds'.format(sound_name)
 
+        if self.current_player:
+            return 'Something is already playing.'
+
         try:
-            player = await voice_client.create_ytdl_player(
+            player = await self.voice_client.create_ytdl_player(
                 sound.get('url'),
                 use_avconv=USE_AVCONV,
-                options='-af "volume={}"'.format(sound.get('volume', 1.0))
             )
+            player.volume = sound.get('volume', 1.0)
             player.start()
+            self.current_player = player
         except youtube_dl.utils.DownloadError as e:
             return str(e)
 
@@ -131,3 +151,82 @@ class CommandSounds(Command):
             response += "\n  **{}**: <{}>".format(key, value['url'])
 
         return response
+
+
+@registry.register('play', sound=True)
+class CommandPlay(SoundCommand):
+    help_text = 'Plays the video from the provided YouTube link in a voice channel or continues a paused video'
+
+    async def execute_sound(self):
+        if not self.arguments or len(self.arguments) < 1:
+            if self.current_player:
+                self.current_player.resume()
+                return 'Unpaused'
+            else:
+                return 'No video running and no link specified'
+
+        if self.current_player:
+            self.current_player.stop()
+
+        url = self.arguments[0]
+        try:
+            player = await self.voice_client.create_ytdl_player(
+                url,
+                use_avconv=USE_AVCONV,
+                after=self._unset_current_player,
+            )
+            player.volume = _current_volume
+            player.start()
+            self.current_player = player
+        except youtube_dl.utils.DownloadError as e:
+            return str(e)
+
+
+@registry.register('pause', sound=True)
+class CommandPause(SoundCommand):
+    help_text = 'Pauses the current YouTube video'
+
+    async def execute_sound(self):
+        if not self.current_player:
+            return 'No video playing'
+
+        self.current_player.pause()
+
+        return 'Paused'
+
+
+@registry.register('stop', sound=True)
+class CommandStop(SoundCommand):
+    help_text = 'Stops the current YouTube video'
+
+    async def execute_sound(self):
+        if not self.current_player:
+            return 'No video playing'
+
+        self.current_player.stop()
+
+        return 'Stopped'
+
+
+@registry.register('volume', sound=True)
+class CommandVolume(SoundCommand):
+    help_text = 'Sets the volume for YouTube videos to x%. Default is 100%'
+    required_arguments = ['volume in %']
+
+    async def execute_sound(self):
+        percent_str = self.arguments[0]
+
+        try:
+            percent = int(percent_str)
+        except ValueError:
+            return
+
+        if 0 < percent > 100:
+            return
+
+        volume = percent / 100
+
+        global _current_volume  # I'm sooo sorry...
+        _current_volume = volume
+        if self.current_player:
+            self.current_player.volume = volume
